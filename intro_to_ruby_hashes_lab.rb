7ec8e497e0379e2cb0d9b2f90d8de555bcbe5d16
Parada Richardson <paradafaii28@gmail.com>
2019-12-12 02:27:25 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,29 +1,48 @@
 def new_hash
-  # return an empty hash
+  num = Hash.new(0)
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  person = {name:'Alice', ages '28'}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer {name:'Grace Hopper'}
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  id_generator = {:id => 'scaintist'}
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  my_ash_creator.keys
+=> ["Bob", "Steve", "Joe"]
 end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
+  name_and_age = { "Bob" => 42, "Steve" => 31, "Joe" => 19}
+=> {"Bob"=>42, "Steve"=>31, "Joe"=>19}
 end
 
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
+  
+  shipping_manifest = {
+  "whale bone corset" => 5,
+  "porcelain vase" => 2,
+  "oil painting" => 3,
+  "silverware" => 34,
+  "loom" => 1
+}
+  
+  if shipping_manifest["top hat"]
+  shipping_manifest["top hat"] += 1
+else
+  shipping_manifest["top hat"] = 1
 end
